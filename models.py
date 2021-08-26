3a04b9f81a86cdd7a48016528264691800c12a45
Francis Beckert <fbeckert@splunk.com>
2021-08-25 17:07:58 -0700

    frontend

--- models.py
+++ models.py
@@ -1,184 +1,215 @@
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from passlib.hash import pbkdf2_sha256 as sha256
 from datetime import datetime
 
 db = SQLAlchemy()
 
+
 class UserModel(db.Model):
-  __tablename__ = "users"
+    __tablename__ = "users"
 
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(12), unique=True, nullable=False)
-  password = db.Column(db.String(120), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(12), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
-  def save_to_db(self):
-    db.session.add(self)
-    db.session.commit()
-
-  @staticmethod
-  def generate_hash(password):
-    return sha256.hash(password)
-
-  @staticmethod
-  def verify_hash(password, hash):
-    return sha256.verify(password, hash)
-
-  @classmethod
-  def find_by_username(cls, username):
-    return cls.query.filter_by(username = username).first()
-
-  @classmethod
-  def return_all(cls):
-    def to_json(x):
-      return {
-        "username": x.username,
-        "password": x.password
-      }
-    return {"users": [to_json(x) for x in UserModel.query.all()]}
-
-  @classmethod
-  def delete_all(cls):
-    try:
-        num_rows_deleted = db.session.query(cls).delete()
+    def save_to_db(self):
+        db.session.add(self)
         db.session.commit()
-        return {"message": "{} row(s) deleted".format(num_rows_deleted)}
-    except:
-      return {"message": "Something went wrong."}
+
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+
+    @staticmethod
+    def verify_hash(password, hash):
+        return sha256.verify(password, hash)
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def return_all(cls):
+        def to_json(x):
+            return {
+                "username": x.username,
+                "password": x.password
+            }
+
+        return {"users": [to_json(x) for x in UserModel.query.all()]}
+
+    @classmethod
+    def delete_all(cls):
+        try:
+            num_rows_deleted = db.session.query(cls).delete()
+            db.session.commit()
+            return {"message": "{} row(s) deleted".format(num_rows_deleted)}
+        except:
+            return {"message": "Something went wrong."}
+
 
 class RevokedTokenModel(db.Model):
-  __tablename__ = "revoked_tokens"
+    __tablename__ = "revoked_tokens"
 
-  id = db.Column(db.Integer, primary_key = True)
-  jti = db.Column(db.String(120))
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(120))
 
-  def add(self):
-    db.session.add(self)
-    db.session.commit()
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
 
-  @classmethod
-  def is_jti_blocklisted(cls, jti):
-    query = cls.query.filter_by(jti = jti).first()
-    return bool(query)
+    @classmethod
+    def is_jti_blocklisted(cls, jti):
+        query = cls.query.filter_by(jti=jti).first()
+        return bool(query)
+
 
 class EventModel(db.Model):
-  __tablename__ = "events"
+    __tablename__ = "events"
 
-  id = db.Column(db.Integer, primary_key = True)
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  user = db.relationship('UserModel', backref=db.backref('users', lazy=True))
-  description = db.Column(db.String(500), nullable=False)
-  link = db.Column(db.String(240), nullable=True)
-  address = db.Column(db.String(500), nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('UserModel', backref=db.backref('users', lazy=True))
+    description = db.Column(db.String(500), nullable=False)
+    link = db.Column(db.String(240), nullable=True)
+    address = db.Column(db.String(500), nullable=True)
 
-  created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
-  updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
-  def save_to_db(self):
-    db.session.add(self)
-    db.session.commit()
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
-  @classmethod
-  def find_by_id(cls, ident):
-    def to_json(x):
-      return {
-        "id": x.id,
-        "username": x.user.username,
-        "user_id": x.user_id,
-        "description": x.description,
-        "link": x.link,
-        "address": x.address
-      }
+    @classmethod
+    def find_by_id(cls, ident):
+        def to_json(x):
+            return {
+                "id": x.id,
+                "username": x.user.username,
+                "user_id": x.user_id,
+                "description": x.description,
+                "link": x.link,
+                "address": x.address
+            }
 
-    if type(ident) != int:
-      return { "user": {} }
+        if type(ident) != int:
+            return {"user": {}}
 
-    event = cls.query.filter_by(id= ident).first()
+        event = cls.query.filter_by(id=ident).first()
 
-    if not event:
-      return { "user": {} }
+        if not event:
+            return {"user": {}}
 
-    return {
-      "user": {
-        "id": event.id,
-        "username": event.user.username,
-        "user_id": event.user_id,
-        "description": event.description,
-        "link": event.link,
-        "address": event.address
-      }
-    }
+        return {
+            "user": {
+                "id": event.id,
+                "username": event.user.username,
+                "user_id": event.user_id,
+                "description": event.description,
+                "link": event.link,
+                "address": event.address
+            }
+        }
 
 
 class ProfileModel(db.Model):
-  __tablename__ = "profiles"
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  user = db.relationship('UserModel', backref=db.backref('profiles', lazy=True))
-  name = db.Column(db.String(120), nullable=False)
-  bio = db.Column(db.String(500), nullable=False)
+    __tablename__ = "profiles"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('UserModel', backref=db.backref('profiles', lazy=True))
+    name = db.Column(db.String(120), nullable=False)
+    bio = db.Column(db.String(500), nullable=False)
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def save_to_db(self):
-    db.session.add(self)
-    db.session.commit()
+    @classmethod
+    def find_by_id(cls, ident):
 
-  @classmethod
-  def find_by_id(cls, ident):
+        if type(ident) != int:
+            return {"user": {}}
 
-    if type(ident) != int:
-      return { "user": {} }
+        profile = cls.query.filter_by(id=ident).first()
 
-    profile = cls.query.filter_by(id=ident).first()
+        if not profile:
+            return {"user": {}}
 
-    if not profile:
-      return { "user": {} }
-
-    return {
-      "user": {
-        "id": profile.id,
-        "user_id": profile.user_id,
-        "name": profile.name,
-        "bio": profile.bio
-      }
-    }
+        return {
+            "user": {
+                "id": profile.id,
+                "user_id": profile.user_id,
+                "name": profile.name,
+                "bio": profile.bio
+            }
+        }
 
 
 class BusinessProfileModel(db.Model):
-  __tablename__ = "businessProfiles"
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  user = db.relationship('UserModel', backref=db.backref('businessProfiles', lazy=True))
-  name = db.Column(db.String(120), nullable=False)
-  description = db.Column(db.String(500), nullable=True)
-  location = db.Column(db.String(500), nullable=True)
-  link = db.Column(db.String(120), nullable=True)
-  phone = db.Column(db.String(120), nullable=True)
+    __tablename__ = "businessProfiles"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('UserModel', backref=db.backref('businessProfiles', lazy=True))
+    name = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(500), nullable=True)
+    location = db.Column(db.String(500), nullable=True)
+    link = db.Column(db.String(120), nullable=True)
+    phone = db.Column(db.String(120), nullable=True)
+    tags = db.Column(db.String(120), nullable=True)
+    picture = db.Column(db.String, nullable=True)
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def save_to_db(self):
-    db.session.add(self)
-    db.session.commit()
+    @classmethod
+    def find_by_id(cls, ident):
 
-  @classmethod
-  def find_by_id(cls, ident):
+        if type(ident) != int:
+            return {"business": {}}
 
-    if type(ident) != int:
-      return { "business": {} }
+        profile = cls.query.filter_by(id=ident).first()
 
-    profile = cls.query.filter_by(id=ident).first()
+        if not profile:
+            return {"business": {}}
 
-    if not profile:
-      return { "business": {} }
+        return {
+            "business": {
+                "id": profile.id,
+                "user_id": profile.user_id,
+                "name": profile.name,
+                "description": profile.description,
+                "location": profile.location,
+                "link": profile.link,
+                "phone": profile.phone,
+                "tags": profile.tags,
+                "picture": profile.picture
+            }
+        }
 
-    return {
-      "business": {
-        "id": profile.id,
-        "user_id": profile.user_id,
-        "name": profile.name,
-        "description": profile.description,
-        "location": profile.location,
-        "link": profile.link,
-        "phone": profile.phone
-      }
-    }
+    @classmethod
+    def get_all(cls):
 
+        profile = cls.query.all()
+
+        if not profile:
+            return {"business": {}}
+
+        profile_list = []
+        for i in range(len(profile)):
+            profile_list.append(
+                {
+                    "id": profile[i].id,
+                    "user_id": profile[i].user_id,
+                    "name": profile[i].name,
+                    "description": profile[i].description,
+                    "location": profile[i].location,
+                    "link": profile[i].link,
+                    "phone": profile[i].phone,
+                    "tags": profile[i].tags
+                }
+            )
+        print(profile_list)
+        return profile_list
--- resources.py
+++ resources.py
@@ -7,90 +7,95 @@ from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_r
 
 # @jwt_refresh_token_required is now @jwt_required(refresh=True)
 # Renamed get_raw_jwt() to get_jwt()
+from sqlalchemy import Text
+
 from models import UserModel, RevokedTokenModel, EventModel, ProfileModel, BusinessProfileModel
 
 login_parser = reqparse.RequestParser()
 login_parser.add_argument("username", help="This field cannot be blank", required=True)
 login_parser.add_argument("password", help="This field cannot be blank", required=True)
 
+
 class UserRegistration(Resource):
-  def post(self):
-    data = login_parser.parse_args()
+    def post(self):
+        data = login_parser.parse_args()
 
-    if UserModel.find_by_username(data["username"]):
-      return {"message": "User {} already exists.".format(data["username"]) }
+        if UserModel.find_by_username(data["username"]):
+            return {"message": "User {} already exists.".format(data["username"])}
 
-    new_user = UserModel(
-      username= data["username"],
-      password= UserModel.generate_hash(data["password"])
-    )
+        new_user = UserModel(
+            username=data["username"],
+            password=UserModel.generate_hash(data["password"])
+        )
 
-    try:
-      new_user.save_to_db()
-      access_token = create_access_token(identity=data["username"])
-      refresh_token = create_refresh_token(identity=data["username"])
-      return {
-        "message": "User {} was created.".format( data["username"] ),
-        "access_token": access_token,
-        "refresh_token": refresh_token
-      }
-    except:
-      return { "message": "Something went wrong!" }, 500
+        try:
+            new_user.save_to_db()
+            access_token = create_access_token(identity=data["username"])
+            refresh_token = create_refresh_token(identity=data["username"])
+            return {
+                "message": "User {} was created.".format(data["username"]),
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }
+        except:
+            return {"message": "Something went wrong!"}, 500
+
 
 class UserLogin(Resource):
-  def post(self):
-    data = login_parser.parse_args()
+    def post(self):
+        data = login_parser.parse_args()
 
-    curr_user = UserModel.find_by_username(data["username"])
+        curr_user = UserModel.find_by_username(data["username"])
 
-    if not curr_user or not UserModel.verify_hash(data["password"], curr_user.password):
-      return {"message": "Wrong credentials"}
+        if not curr_user or not UserModel.verify_hash(data["password"], curr_user.password):
+            return {"message": "Wrong credentials"}
 
-    access_token = create_access_token(identity=data["username"])
-    refresh_token = create_refresh_token(identity=data["username"])
+        access_token = create_access_token(identity=data["username"])
+        refresh_token = create_refresh_token(identity=data["username"])
 
+        return {
+            "message": "Logged in as {}".format(curr_user.username),
+            "access_token": access_token,
+            "refresh_token": refresh_token
+        }
 
-    return {
-      "message": "Logged in as {}".format(curr_user.username),
-      "access_token": access_token,
-      "refresh_token": refresh_token
-    }
 
 class UserLogoutAccess(Resource):
-  @jwt_required()
-  def post(self):
-    jti = get_jwt()["jti"]
-    try:
-      revoked_token = RevokedTokenModel(jti = jti)
-      revoked_token.add()
-      return {"message": "Access token has been revoked"}
-    except:
-      return {"message": "Something went wrong"}, 500
-      
-      
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        try:
+            revoked_token = RevokedTokenModel(jti=jti)
+            revoked_token.add()
+            return {"message": "Access token has been revoked"}
+        except:
+            return {"message": "Something went wrong"}, 500
+
+
 class UserLogoutRefresh(Resource):
-  @jwt_required(refresh=True)
-  def post(self):
-    jti = get_jwt()["jti"]
-    try:
-      revoked_token = RevokedTokenModel(jti = jti)
-      revoked_token.add()
-      return {"message": "Refresh token has been revoked"}
-    except:
-      return {"message": "Something went wrong"}, 500
-      
-      
+    @jwt_required(refresh=True)
+    def post(self):
+        jti = get_jwt()["jti"]
+        try:
+            revoked_token = RevokedTokenModel(jti=jti)
+            revoked_token.add()
+            return {"message": "Refresh token has been revoked"}
+        except:
+            return {"message": "Something went wrong"}, 500
+
+
 class TokenRefresh(Resource):
     @jwt_required(refresh=True)
     def post(self):
-      curr_user = get_jwt_identity()
-      access_token = create_access_token(identity=curr_user)
-      return {"access_token": access_token}
-      
-      
+        curr_user = get_jwt_identity()
+        access_token = create_access_token(identity=curr_user)
+        return {"access_token": access_token}
+
+
 class AllUsers(Resource):
-  def get(self):
-    return UserModel.return_all()
+    def get(self):
+        return UserModel.return_all()
+
 
 event_parser = reqparse.RequestParser()
 event_parser.add_argument("id", type=int, help="This field cannot be blank", required=True)
@@ -98,37 +103,39 @@ new_event_parser = reqparse.RequestParser()
 new_event_parser.add_argument("description", help="This field cannot be blank", required=True)
 new_event_parser.add_argument("link", help="This field cannot be blank", required=False)
 new_event_parser.add_argument("address", help="This field cannot be blank", required=False)
+
+
 class EventResource(Resource):
-  def get(self):
-    data = event_parser.parse_args()
+    def get(self):
+        data = event_parser.parse_args()
 
-    try:
-      ident = int(data["id"])
-      event = EventModel.find_by_id(ident)
-      return event
-    except:
-      return { "message": "Something went wrong." }, 500
-  
-  @jwt_required()
-  def post(self):
-    data = new_event_parser.parse_args()
-    username = get_jwt_identity()
-    curr_user = UserModel.find_by_username(username)
+        try:
+            ident = int(data["id"])
+            event = EventModel.find_by_id(ident)
+            return event
+        except:
+            return {"message": "Something went wrong."}, 500
 
-    try:
-      traceback.print_exc()
-      event = EventModel(
-        user=curr_user,
-        description=data["description"],
-        link=data["link"],
-        address=data["address"]
-      )
+    @jwt_required()
+    def post(self):
+        data = new_event_parser.parse_args()
+        username = get_jwt_identity()
+        curr_user = UserModel.find_by_username(username)
 
-      event.save_to_db()
+        try:
+            traceback.print_exc()
+            event = EventModel(
+                user=curr_user,
+                description=data["description"],
+                link=data["link"],
+                address=data["address"]
+            )
 
-      return { "message": "Success. User {} created event {}".format(curr_user.username, event.id) }
-    except:
-      return { "message": "Something went run." }, 500
+            event.save_to_db()
+
+            return {"message": "Success. User {} created event {}".format(curr_user.username, event.id)}
+        except:
+            return {"message": "Something went run."}, 500
 
 
 profile_parser = reqparse.RequestParser()
@@ -139,80 +146,85 @@ new_profile_parser.add_argument("bio", help="This field cannot be blank", requir
 
 
 class ProfileResource(Resource):
-  def get(self):
-    data = profile_parser.parse_args()
+    def get(self):
+        data = profile_parser.parse_args()
 
-    try:
-      ident = int(data["id"])
-      profile = ProfileModel.find_by_id(ident)
-      return profile
-    except:
-      return {"message": "Something went run."}, 500
+        try:
+            ident = int(data["id"])
+            profile = ProfileModel.find_by_id(ident)
+            return profile
+        except:
+            return {"message": "Something went run."}, 500
 
-  @jwt_required()
-  def post(self):
-    data = new_profile_parser.parse_args()
-    username = get_jwt_identity()
-    curr_user = UserModel.find_by_username(username)
-    print(curr_user.username)
-    print(data)
+    @jwt_required()
+    def post(self):
+        data = new_profile_parser.parse_args()
+        username = get_jwt_identity()
+        curr_user = UserModel.find_by_username(username)
+        print(curr_user.username)
+        print(data)
 
-    try:
-      print(curr_user, data["name"], data["bio"], data["profile_pic"])
-      profile = ProfileModel(
-        user=curr_user,
-        name=data["name"],
-        bio=data["bio"]
-      )
-      profile.save_to_db()
+        try:
+            print(curr_user, data["name"], data["bio"], data["profile_pic"])
+            profile = ProfileModel(
+                user=curr_user,
+                name=data["name"],
+                bio=data["bio"]
+            )
+            profile.save_to_db()
 
-      return {"message": "Success. User {} created profile {}".format(curr_user.username, profile.id)}
-    except:
-      return {"message": "Something went wrong."}, 500
+            return {"message": "Success. User {} created profile {}".format(curr_user.username, profile.id)}
+        except:
+            return {"message": "Something went wrong."}, 500
 
 
 business_profile_parser = reqparse.RequestParser()
 business_profile_parser.add_argument("id", type=int, help="This field cannot be blank", required=True)
+
 new_business_profile_parser = reqparse.RequestParser()
 new_business_profile_parser.add_argument("name", help="This field cannot be blank", required=True)
 new_business_profile_parser.add_argument("description", help="Must be a string", required=False)
 new_business_profile_parser.add_argument("location", help="Must be a string", required=False)
 new_business_profile_parser.add_argument("link", help="Must be a string", required=False)
 new_business_profile_parser.add_argument("phone", help="Must be a string", required=False)
+new_business_profile_parser.add_argument("tags", help="Must be a string", required=False)
+new_business_profile_parser.add_argument("picture", help="Must be a string", required=False)
 
 
 class BusinessProfileResource(Resource):
-  def get(self):
-    data = profile_parser.parse_args()
+    def get(self):
+        data = profile_parser.parse_args()
+        try:
+            ident = int(data["id"])
+            if ident == 0:
+                profile = BusinessProfileModel.get_all()
+            else:
+                profile = BusinessProfileModel.find_by_id(ident)
+            return profile
+        except:
+            return {"message": "Something went run."}, 500
 
-    try:
-      ident = int(data["id"])
-      profile = BusinessProfileModel.find_by_id(ident)
-      return profile
-    except:
-      return {"message": "Something went run."}, 500
+    @jwt_required()
+    def post(self):
+        data = new_business_profile_parser.parse_args()
+        username = get_jwt_identity()
+        curr_user = UserModel.find_by_username(username)
 
-  @jwt_required()
-  def post(self):
-    data = new_business_profile_parser.parse_args()
-    username = get_jwt_identity()
-    curr_user = UserModel.find_by_username(username)
-    print(curr_user.username)
+        try:
 
-    try:
-      print(data)
+            profile = BusinessProfileModel(
+                user=curr_user,
+                name=data["name"],
+                description=data["description"],
+                location=data["location"],
+                link=data["link"],
+                phone=data["phone"],
+                tags=data["tags"],
+                picture=data["picture"]
+            )
 
-      profile = BusinessProfileModel(
-        user=curr_user,
-        name=data["name"],
-        description=data["description"],
-        location=data["location"],
-        link=data["link"],
-        phone=data["phone"]
-      )
+            profile.save_to_db()
 
-      profile.save_to_db()
-
-      return {"message": "Success. User {} created business profile {}".format(curr_user.username, profile.id)}
-    except:
-      return {"message": "Something went, wrong."}, 500
+            return {"message": "Success. User {} created business profile {}".format(curr_user.username, profile.id)}
+        except:
+            return {"message": "Something went, wrong."}, 500
